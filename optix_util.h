cb6d970625b2312f263ceea963683a85a0e22326
shocker-0x15 <shocker.0x15@gmail.com>
2020-03-21 20:32:20 +0900

    backup commit.   removed SlotFinder.

--- optix_util.h
+++ optix_util.h
@@ -5,10 +5,7 @@
 現状ではあらゆるAPIに破壊的変更が入る可能性が非常に高い。
 
 TODO:
-- ASのダブルバッファリングの運用方法を考える。
-- update()の運用方法を考える。
 - SBTのダブルバッファリングの運用方法を考える。
-- SlotFinderはこのまま必要ないなら削除する。
 - Assertとexceptionの整理。
 
 */
@@ -98,115 +95,6 @@ namespace optix {
 
 
 #if !defined(__CUDA_ARCH__)
-    class SlotFinder {
-        uint32_t m_numLayers;
-        uint32_t m_numLowestFlagBins;
-        uint32_t m_numTotalCompiledFlagBins;
-        uint32_t* m_flagBins;
-        uint32_t* m_offsetsToOR_AND;
-        uint32_t* m_numUsedFlagsUnderBinList;
-        uint32_t* m_offsetsToNumUsedFlags;
-        uint32_t* m_numFlagsInLayerList;
-
-        SlotFinder(const SlotFinder &) = delete;
-        SlotFinder &operator=(const SlotFinder &) = delete;
-
-        void aggregate();
-    public:
-        static constexpr uint32_t InvalidSlotIndex = 0xFFFFFFFF;
-
-        SlotFinder() :
-            m_numLayers(0), m_numLowestFlagBins(0), m_numTotalCompiledFlagBins(0),
-            m_flagBins(nullptr), m_offsetsToOR_AND(nullptr),
-            m_numUsedFlagsUnderBinList(nullptr), m_offsetsToNumUsedFlags(nullptr),
-            m_numFlagsInLayerList(nullptr) {
-        }
-        ~SlotFinder() {
-        }
-
-        void initialize(uint32_t numSlots);
-
-        void finalize();
-
-        SlotFinder &operator=(SlotFinder &&inst) {
-            finalize();
-
-            m_numLayers = inst.m_numLayers;
-            m_numLowestFlagBins = inst.m_numLowestFlagBins;
-            m_numTotalCompiledFlagBins = inst.m_numTotalCompiledFlagBins;
-            m_flagBins = inst.m_flagBins;
-            m_offsetsToOR_AND = inst.m_offsetsToOR_AND;
-            m_numUsedFlagsUnderBinList = inst.m_numUsedFlagsUnderBinList;
-            m_offsetsToNumUsedFlags = inst.m_offsetsToNumUsedFlags;
-            m_numFlagsInLayerList = inst.m_numFlagsInLayerList;
-            inst.m_flagBins = nullptr;
-            inst.m_offsetsToOR_AND = nullptr;
-            inst.m_numUsedFlagsUnderBinList = nullptr;
-            inst.m_offsetsToNumUsedFlags = nullptr;
-            inst.m_numFlagsInLayerList = nullptr;
-
-            return *this;
-        }
-        SlotFinder(SlotFinder &&inst) {
-            *this = std::move(inst);
-        }
-
-        void resize(uint32_t numSlots);
-
-        void reset() {
-            std::fill_n(m_flagBins, m_numLowestFlagBins + m_numTotalCompiledFlagBins, 0);
-            std::fill_n(m_numUsedFlagsUnderBinList, m_numLowestFlagBins + m_numTotalCompiledFlagBins / 2, 0);
-        }
-
-        uint32_t getNumLayers() const {
-            return m_numLayers;
-        }
-
-        const uint32_t* getOffsetsToOR_AND() const {
-            return m_offsetsToOR_AND;
-        }
-
-        const uint32_t* getOffsetsToNumUsedFlags() const {
-            return m_offsetsToNumUsedFlags;
-        }
-
-        const uint32_t* getNumFlagsInLayerList() const {
-            return m_numFlagsInLayerList;
-        }
-
-
-
-        void setInUse(uint32_t slotIdx);
-
-        void setNotInUse(uint32_t slotIdx);
-
-        bool getUsage(uint32_t slotIdx) const {
-            uint32_t binIdx = slotIdx / 32;
-            uint32_t flagIdxInBin = slotIdx % 32;
-            uint32_t flagBin = m_flagBins[binIdx];
-
-            return (bool)((flagBin >> flagIdxInBin) & 0x1);
-        }
-
-        uint32_t getFirstAvailableSlot() const;
-
-        uint32_t getFirstUsedSlot() const;
-
-        uint32_t find_nthUsedSlot(uint32_t n) const;
-
-        uint32_t getNumSlots() const {
-            return m_numFlagsInLayerList[0];
-        }
-
-        uint32_t getNumUsed() const {
-            return m_numUsedFlagsUnderBinList[m_offsetsToNumUsedFlags[m_numLayers - 1]];
-        }
-
-        void debugPrint() const;
-    };
-
-
-
     using namespace CUDAHelper;
 
     /*
