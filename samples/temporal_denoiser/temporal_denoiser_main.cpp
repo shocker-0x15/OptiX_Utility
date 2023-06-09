e0fd70074bb8a8af28fd7b17dbad8d32f1d618aa
Shin Watanabe <shin.watanabe@sony.com>
2023-06-09 09:15:27 +0900

    fixes some samples that used old code.

--- samples/temporal_denoiser/temporal_denoiser_main.cpp
+++ samples/temporal_denoiser/temporal_denoiser_main.cpp
@@ -62,14 +62,11 @@ int32_t main(int32_t argc, const char* argv[]) try {
     // EN: Settings for OptiX context and pipeline.
 
     CUcontext cuContext;
-    int32_t cuDeviceCount;
-    StreamChain<2> streamChain;
+    CUstream stream;
     CUDADRV_CHECK(cuInit(0));
-    CUDADRV_CHECK(cuDeviceGetCount(&cuDeviceCount));
     CUDADRV_CHECK(cuCtxCreate(&cuContext, 0, 0));
     CUDADRV_CHECK(cuCtxSetCurrent(cuContext));
-    streamChain.initialize(cuContext);
-    CUstream stream = streamChain.waitAvailableAndGetCurrentStream();
+    CUDADRV_CHECK(cuStreamCreate(&stream, 0));
 
     optixu::Context optixContext = optixu::Context::create(cuContext);
 
