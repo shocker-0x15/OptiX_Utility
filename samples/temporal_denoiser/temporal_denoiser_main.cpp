803c8be061a5b04e1e08d7742e59ec3acf938c7d
shocker-0x15 <shocker.0x15@gmail.com>
2025-02-11 18:22:54 +0900

    denoiser, temporal denoiser: removed outdated comments.

--- samples/temporal_denoiser/temporal_denoiser_main.cpp
+++ samples/temporal_denoiser/temporal_denoiser_main.cpp
@@ -5,7 +5,6 @@ JP: このサンプルはテンポラルデノイザーの使用方法を示し�
     テンポラルデノイザーはアルベドや法線に加えて、前フレームのデノイズ済みビューティー、
     ピクセルごとの対応を示すフローチャンネルを補助画像として受け取ります。
 
-    --kp: カーネル予測モデルを使用する。
     --upscale: アップスケールも実行する。
 
 EN: This sample shows how to use the temporal denoiser.
@@ -13,7 +12,6 @@ EN: This sample shows how to use the temporal denoiser.
     The temporal denoiser takes the denoised beauty of the previous frame and a flow channel indicating
     per-pixel correspondance as auxiliary images in addition to albedo and normal.
 
-    --kp: Use a kernel prediction model.
     --upscale: Perform upscaling.
 
 */
