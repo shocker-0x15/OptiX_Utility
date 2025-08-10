cc9163a46687fd72e83f188d37b0a99498307329
shocker-0x15 <shocker.0x15@gmail.com>
2025-08-10 22:33:25 +0900

    updated the CUDA version to 13.0.

--- tests/optixu_tests/main.cpp
+++ tests/optixu_tests/main.cpp
@@ -1001,7 +1001,12 @@ int32_t main(int32_t argc, const char* argv[]) {
     ::testing::InitGoogleTest(&argc, const_cast<char**>(argv));
 
     CUDADRV_CHECK(cuInit(0));
+#if CUDA_VERSION < 13000
     CUDADRV_CHECK(cuCtxCreate(&cuContext, 0, 0));
+#else
+    CUctxCreateParams cuCtxCreateParams = {};
+    CUDADRV_CHECK(cuCtxCreate(&cuContext, &cuCtxCreateParams, 0, 0));
+#endif
     CUDADRV_CHECK(cuCtxSetCurrent(cuContext));
     CUDADRV_CHECK(cuStreamCreate(&cuStream, 0));
 
